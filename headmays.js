7588b32acb52ac5e1efdb5b33c29744a75f9cd8f
guebieun <83795312+guebieun@users.noreply.github.com>
2021-05-07 08:32:20 +0700

    Create headmays.js

---
+++ headmays.js
@@ -0,0 +1,19 @@
+var images = [],
+index = 0;
+
+images[0] = "<b> ISTRI SELINGKUH VIRAL </b> <a href = 'https://ads1.guebieun.com' target='blank'><img src='https://1.bp.blogspot.com/-F5NDKR_c3rc/YJSGJrEeW0I/AAAAAAAAEjA/nBAwl50dpNsCSUv_XgvHe4YsLypcr2SBQCLcBGAsYHQ/s16000/ms_puiyi_175816099_295044445330389_3876018797853865512_n.jpg' alt='Click here to watch'></a>";
+
+images[1] = "<b> MAHASISWA DIGENJOT DOSEN </b> <a href = 'https://ads1.guebieun.com' target='blank'><img src='https://1.bp.blogspot.com/-aVNx6_s_U2o/YJSGHeYE5EI/AAAAAAAAEis/o0JZnx08WEAr_N2-uHAVNyURdVHhG8zlQCLcBGAsYHQ/s16000/ms_puiyi_155112338_461793574868219_4826974072203712565_n.jpg' alt='Click here to watch'></a>";
+
+images[2] = "<b> KETAHUAN AYAH SAAT NGOCOK </b> <a href = 'https://ads1.guebieun.com' target='blank'><img src='https://1.bp.blogspot.com/-dEjHE4FaqJs/YJSXIyJLm0I/AAAAAAAAEkY/TsLR2BeBY-kcdn6k6wprGEoz5PEFGk9zwCLcBGAsYHQ/s16000/byeol_33_144665633_1323895884648307_7103730521369131806_n.jpg' alt='Click here to watch'></a>";
+
+images[3] = "<b> VIDEO TANTE CINA CANTIK </b> <a href = 'https://ads1.guebieun.com' target='blank'><img src='https://1.bp.blogspot.com/-wckWZi4yXQ8/YJSSCxpztiI/AAAAAAAAEj4/eToSGxY4Agoa4k-mjY5BZiYMwUZTr32SACLcBGAsYHQ/s16000/sexi_girls_chinese_91515785_573458676600612_1390531747628217109_n.jpg' alt='Click here to watch'></a>";
+
+images[4] = "<b> GADIS SMA MAIN SAMA GURU </b> <a href = 'https://ads1.guebieun.com' target='blank'><img src='https://1.bp.blogspot.com/-ymfmDXFxQAc/YJSSD8atDfI/AAAAAAAAEkE/vBfKn16Z8Xodfb38ugLGfGcMsYGOqwV9gCLcBGAsYHQ/s16000/sexi_girls_chinese_92473465_2577725195831995_3076064614404461732_n.jpg' alt='Click here to watch'></a>";
+
+images[5] = "<b> ABG IMUT DIGENJOT DARI BELAKANG </b> <a href = 'https://ads1.guebieun.com' target='blank'><img src='https://1.bp.blogspot.com/-aqyLUFKhfWA/YJSSC9HCt1I/AAAAAAAAEj8/qK1rY8crGNUk7mCVtI6WBMSf8EZxlaLMwCLcBGAsYHQ/s16000/sexi_girls_chinese_108555737_992827987823596_2782518641758155489_n.jpg' alt='Click here to watch'></a>";
+
+
+index = Math.floor(Math.random() * images.length);
+document.write(images[index]);
+//done
