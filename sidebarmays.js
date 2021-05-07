1a0afbf39396939a10a8123588afcbb70ccdcb20
guebieun <83795312+guebieun@users.noreply.github.com>
2021-05-08 05:18:04 +0700

    Create sidebarmays.js

---
+++ sidebarmays.js
@@ -0,0 +1,14 @@
+var images = [],
+index = 0;
+
+images[0] = "<a href = 'https://ads2.guebieun.com' target='blank'><img src='https://1.bp.blogspot.com/-BWsHAxVsADY/X3YV9DLEVJI/AAAAAAAADh8/rBKaiBqR7yolpeUD5sQk1UyfQbEvr4SKQCLcBGAsYHQ/s300/sectret.jpeg' alt='Click here to download'></a>";
+images[1] = "<a href = 'https://ads2.guebieun.com' target='blank'><img src='https://1.bp.blogspot.com/-VEqNgg1EijU/X3NUpqgm03I/AAAAAAAABjU/dgI8_xOPzBgCJn1_tzRNi2__uCMLGBlFACLcBGAsYHQ/s0/-2.jpg' alt='Click here to download'></a>";
+images[2] = "<a href = 'https://ads2.guebieun.com' target='blank'><img src='https://1.bp.blogspot.com/-789Xi_zH6vc/X3NUpc7Q5wI/AAAAAAAABjQ/W2atPMMB93o9qVLIn-K0GEBdvQUfF99swCLcBGAsYHQ/s0/-3.jpg' alt='Click here to download'></a>";
+images[3] = "<a href = 'https://ads2.guebieun.com' target='blank'><img src='https://1.bp.blogspot.com/-aA0mqC-KpHM/X3NUqee7KrI/AAAAAAAABjc/GJT0T3aDVQ8mRSHZ5LmLEJBddiMpgC33gCLcBGAsYHQ/s0/-4.jpg' alt='Click here to download'></a>";
+images[4] = "<a href = 'https://ads2.guebieun.com' target='blank'><img src='https://1.bp.blogspot.com/-on-H1JbRRYg/X3NUqia88nI/AAAAAAAABjg/cm2TmA3d8qUB-Vc-zvbRZvGtVq7LjR8ygCLcBGAsYHQ/s0/-5.jpg' alt='Click here to download'></a>";
+images[5] = "<a href = 'https://ads2.guebieun.com' target='blank'><img src='https://1.bp.blogspot.com/-NLwKuuJbjZc/X3NUqzfXzTI/AAAAAAAABjk/edaJKgMu3jArc9c1KpnZ9as2Mau8Lx8uQCLcBGAsYHQ/s0/6-.jpg' alt='Click here to download'></a>";
+
+
+index = Math.floor(Math.random() * images.length);
+document.write(images[index]);
+//done
